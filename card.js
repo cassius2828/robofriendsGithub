e6dfcac6cbd133e9e78062ddde70de972dc92959
Cassius Reynolds <cassius2828@gmail.com>
2023-06-22 21:35:51 -0700

    deleted test runs, started making robo app

---
+++ card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = () => {
+    return (
+        <div>
+            <img src="" alt="robo profile pic" />
+            <div>
+                <h2>Jane Dow</h2>
+                <p>jandow@yahoo.com</p>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import Hello from './Hello';
+import Card from './Card';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Hello />
-    <App />
+    <Card/>
   </React.StrictMode>
 );
 
