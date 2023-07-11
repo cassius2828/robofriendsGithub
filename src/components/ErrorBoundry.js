2afd2625660e53285ca36800f84c031b6ed7d86c
Cassius Reynolds <cassius2828@gmail.com>
2023-07-10 20:38:27 -0700

    practiced boundry errors

--- src/components/ErrorBoundry.js
+++ src/components/ErrorBoundry.js
@@ -1,23 +1,20 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 class ErrorBoundry extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            hasError: false
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasError: false,
+    };
+  }
 
-componentDidCatch(error, info) {
-    this.setState({hasError: true})
-}
+  componentDidCatch(error, info) {
+    this.setState({ hasError: true });
+  }
 
-    render() {
-        if(this.state.hasError) {
-            return <h1>Oopsies</h1>
-        }
-        return this.props.children
-    }
+  render() {
+    return this.state.hasError ? <h1>Error Occured</h1> : this.props.children;
+  }
 }
 
 export default ErrorBoundry;
